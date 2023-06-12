7adfa4ccaf5ce65b69e5de358d3c783e4aa0f65d
Deni Lark <denlarsites@gmail.com>
2023-06-12 22:53:48 +0500

    prepare redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import { CssBaseline } from "@mui/material"
+import store from "./store/store"
+import { Provider } from "react-redux"
 
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 ).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
   </React.StrictMode>
 )
---
+++ src/store/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit"
+import app from "./slices/appSlice"
+
+const store = configureStore({
+  reducer: { app },
+})
+
+export default store
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
